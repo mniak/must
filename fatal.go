ff98aec58d0cc625669023ed9f3029f98dbccdad
Andre Soares <andregsilv@gmail.com>
2022-12-06 13:00:39 -0300

    Make fatal settable

--- fatal.go
+++ fatal.go
@@ -1,8 +1,20 @@
 package must
 
-import "log"
+import (
+	"log"
+)
 
-func fatal(err error) {
+var fatal func(err error) = DefaultFatal
+
+func SetFatalFunc(fn func(err error)) {
+	fatal = fn
+}
+
+func ResetFatalFunc() {
+	fatal = DefaultFatal
+}
+
+func DefaultFatal(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
